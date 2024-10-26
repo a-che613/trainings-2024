0743ac7740e099b4020d9d2375eeb7bca9067c15
a-che613 <alexche613@gmail.com>
2024-10-26 15:59:05 +0100

    made changes

--- Python/Week 1/contact-book/functions/add_contact.py
+++ Python/Week 1/contact-book/functions/add_contact.py
@@ -1,54 +1,46 @@
+import re
 from contact_handlers import save_contacts
+addMoreContacts = True
 
 def addContacts(contacts):
-  addMoreContacts = True
-  addOptions = {'1': 'Save', '2': 'Add more'}
-  # contacts = get_contacts();
-
+  
   while addMoreContacts:
     
-    # this try-exception handles validation errors, for instance "strings" in the number field
-    try:
       # receive all the user inputs
-      name = input('Enter the contact name: ')
-      number = int(input('Enter the contact number: '))
-      email = input('Enter the contact email: ')
-
-      if not '@gmail.com' in email:
-        print('Invalid email')
-        raise Exception('Invalid email')
       
-      contacts.append({'name': name, 'number': str(number), 'email': email})
+      new_contact = getUserInput()
+      contacts.append(new_contact)
 
 
-      # end the function if the save option was selected, otherwise handle the error
-      try:
-        response = validateOption(contacts)
-
-        if response['success']:
-          print(response['message'])
-          addMoreContacts = False
-
-      except Exception as e:
-        raise e
-      # end try
+      # either save and exit or add more
+      validateOption(contacts)
 
 
-    except Exception as e:
-      if 'invalid literal for int()' in str(e):
-        print('Please enter a valid phoneNumber e.g 654490031')
-      if str(e) is 'Invalid email':
-        print('Please Enter a valid email')
-    # end try
+
+def getUserInput():
+  email_regex = r'^[a-zA-z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+  name = input('Enter the contact name: ')
+
+  number = input('Enter the contact number: ')
+  while not number.isnumeric():
+    number = input('Enter a valid number e.g 654490031: ')
+
+  email = input('Enter the contact email: ')
+  while not re.match(email_regex, email):
+    email = input('Enter a valid email e.g example@gmail.com: ')
+
+
+  return {'name': name, 'number': str(number), 'email': email}
+
 
 def validateOption(contacts):
+    global addMoreContacts
     option = input('Enter 1 to save or 2 to add more: ')
     if option == '1':
       
       save_contacts(contacts)
-      
-      return {"success": True, "message": "Contact's saved", "data": contacts}
-      
+      print('Contact Saved Successfully')
+      addMoreContacts = False
     elif option == '2':
       # addContacts()
       pass
--- Python/Week 1/contact-book/main.py
+++ Python/Week 1/contact-book/main.py
@@ -7,14 +7,16 @@ from functions.update_contact import updateContact
 
 
 all_contacts = get_contacts()
-print(f'###### These are your contacts ###### \n {all_contacts}')
+keepRunning = True
 
-print('++++++++++++++++++++++++++++++++++')
-print('+++++++++++ PHONE BOOK +++++++++++')
-print('++++++++++++++++++++++++++++++++++')
-print('Enter 1 to add contacts\nEnter 2 to delete a contact and\nEnter 3 to update a contact')
+
 
 def getOption():
+  print("----------------------------")
+  print('------- PHONE BOOK ---------')
+  print("----------------------------")
+
+  print('Enter 1 to add contacts\nEnter 2 to delete a contact\nEnter 3 to update a contact\nEnter 4 to view all contacts and\nEnter 5 to exit')
   option = input('Which option do you choose? (enter a valid option): ')
 
   if option == '1':
@@ -23,11 +25,20 @@ def getOption():
     deleteContact()
   elif option == '3':
     updateContact(all_contacts)
+  elif option == '4':
+    for contact in all_contacts:
+      print("----------------------------")
+      print(f"Name: {contact['name']}")
+      print(f"Number: {contact['number']}")
+      print(f"Email: {contact['email']}")
+  elif option == '5':
+    keepRunning = False
   else:
     getOption()
 
 
-getOption()
+while keepRunning:
+  getOption()
 
 
 
